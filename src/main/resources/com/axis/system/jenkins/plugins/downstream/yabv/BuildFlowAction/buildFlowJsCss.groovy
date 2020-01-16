30932576a15172ad2aac06e92f8851efa42ae6d0
Gustaf Lundh <gustaflh@axis.com>
2020-01-16 12:05:41 +0100

    Indicate if the cache is still refreshing

--- src/main/resources/com/axis/system/jenkins/plugins/downstream/yabv/BuildFlowAction/buildFlowJsCss.groovy
+++ src/main/resources/com/axis/system/jenkins/plugins/downstream/yabv/BuildFlowAction/buildFlowJsCss.groovy
@@ -1,16 +1,20 @@
 package com.axis.system.jenkins.plugins.downstream.yabv.BuildFlowAction
 
+import com.axis.system.jenkins.plugins.downstream.cache.BuildCache
+
 link(rel: 'stylesheet',
     type: 'text/css',
     href: "${rootURL}/plugin/yet-another-build-visualizer/css/layout.css")
 
 div(id: 'build-flow-root') {
   div(id: 'build-flow-switches')
-  div(id: 'build-flow-grid')
+  div(id: 'build-flow-grid-holder') {
+    noscript() {
+      include(my, 'buildFlow.groovy')
+    }
+  }
   script("buildFlowRefreshInterval='${System.getProperty('yabv.buildFlowRefreshInterval', '10000')}'")
   script(src: "${rootURL}/plugin/yet-another-build-visualizer/scripts/render.js",
       type: 'text/javascript')
-  noscript() {
-    include(my, 'buildFlow.groovy')
-  }
+
 }
