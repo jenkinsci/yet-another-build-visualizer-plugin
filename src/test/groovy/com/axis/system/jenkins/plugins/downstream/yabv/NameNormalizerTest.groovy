7f6b2fafda3c3bf6dee56da250f57bc1fed15ecf
Jan Gałda <jan.galda@nordicsemi.no>
2022-05-13 10:00:02 +0200

    Add button to display Full Build Path

--- src/test/groovy/com/axis/system/jenkins/plugins/downstream/yabv/NameNormalizerTest.groovy
+++ src/test/groovy/com/axis/system/jenkins/plugins/downstream/yabv/NameNormalizerTest.groovy
@@ -58,4 +58,63 @@ class NameNormalizerTest {
     assert nameNormalizer.getNormalizedName(itemB) == 'd/a'
     assert nameNormalizer.getNormalizedName(itemC) == 'b'
   }
+
+  @Test
+  void singleItemNoCollisionsMin2() {
+    def itemA = ['a', 'b', 'root']
+    def nameNormalizer = new NameNormalizer([itemA].toSet(), NAME_FUNC, PARENT_FUNC, true)
+    assert nameNormalizer.getNormalizedName(itemA) == '/b/a'
+  }
+
+  @Test
+  void multipleItemsNoCollisionsMin2() {
+    def itemA = ['a', 'b', 'root']
+    def itemB = ['b', 'b', 'root']
+
+    def nameNormalizer = new NameNormalizer([itemA, itemB].toSet(), NAME_FUNC, PARENT_FUNC, true)
+    assert nameNormalizer.getNormalizedName(itemA) == '/b/a'
+    assert nameNormalizer.getNormalizedName(itemB) == '/b/b'
+  }
+
+  @Test
+  void multipleItemsCollisionsMin2() {
+    def itemA = ['a', 'b', 'root']
+    def itemB = ['a', 'd', 'root']
+
+    def nameNormalizer = new NameNormalizer([itemA, itemB].toSet(), NAME_FUNC, PARENT_FUNC, true)
+    assert nameNormalizer.getNormalizedName(itemA) == '/b/a'
+    assert nameNormalizer.getNormalizedName(itemB) == '/d/a'
+  }
+
+  @Test
+  void longItemsNoCollisionsMin2() {
+    def itemA = ['a', 'b', 'c', 'root']
+    def itemB = ['d', 'e', 'f', 'root']
+
+    def nameNormalizer = new NameNormalizer([itemA, itemB].toSet(), NAME_FUNC, PARENT_FUNC, true)
+    assert nameNormalizer.getNormalizedName(itemA) == '/c/b/a'
+    assert nameNormalizer.getNormalizedName(itemB) == '/f/e/d'
+  }
+
+  @Test
+  void rootCollisionsMin2() {
+    def itemA = ['a', 'b', 'root']
+    def itemB = ['a', 'root']
+
+    def nameNormalizer = new NameNormalizer([itemA, itemB].toSet(), NAME_FUNC, PARENT_FUNC, true)
+    assert nameNormalizer.getNormalizedName(itemB) == '/a'
+    assert nameNormalizer.getNormalizedName(itemA) == '/b/a'
+  }
+
+  @Test
+  void innocentBystanderMin2() {
+    def itemA = ['a', 'b', 'root']
+    def itemB = ['a', 'd', 'root']
+    def itemC = ['b', 'd', 'root']
+
+    def nameNormalizer = new NameNormalizer([itemA, itemB].toSet(), NAME_FUNC, PARENT_FUNC, true)
+    assert nameNormalizer.getNormalizedName(itemA) == '/b/a'
+    assert nameNormalizer.getNormalizedName(itemB) == '/d/a'
+    assert nameNormalizer.getNormalizedName(itemC) == '/d/b'
+  }
 }
